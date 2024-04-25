4b63e0768e9da0d38ad1fa0f5d5bf3ae91fcc385
Chris <capacheco.22@est.ucab.edu.ve>
2024-04-24 23:53:56 -0400

    feat: done

--- src/server/api/routers/cal/checkAvailability.handler.ts
+++ src/server/api/routers/cal/checkAvailability.handler.ts
@@ -51,6 +51,106 @@ const getUserAvailability = async (
   }
 };
 
+interface CreateBookingOptions {
+  calApiKey: string;
+  eventTypeId: number;
+  start: string; // This can be converted to Date if needed
+  end: string; // This can be converted to Date if needed
+  responses: {
+    name: string;
+    email: string;
+    location: string;
+  };
+
+  metadata: object; // or specific metadata type if known
+  timeZone: string;
+  language: string;
+  title: string;
+  description: string | null;
+  status: string;
+  smsReminderNumber: string | null;
+}
+
+// interface CreateBookingOptions {
+//   calApiKey: string;
+//   eventTypeId: number;
+//   start: string;
+//   end: string;
+//   responses: {
+//     name: string;
+//     email: string;
+//     location: string;
+//     metadata: object;
+//   };
+//   language: string;
+//   timeZone: string;
+//   title: string;
+//   recurringEventId: number;
+//   description: string;
+//   status: "ACCEPTED" | "PENDING" | "CANCELLED" | "REJECTED";
+//   seatsPerTimeSlot: number;
+//   seatsShowAttendees: boolean;
+//   seatsShowAvailabilityCount?: boolean;
+//   smsReminderNumber: number | null;
+// }
+
+interface Booking {
+  id: number;
+  userId: number;
+  description: string;
+  eventTypeId: number;
+  uid: string;
+  title: string;
+  startTime: string; // This can be converted to Date if needed
+  endTime: string; // This can be converted to Date if needed
+  attendees: Attendee[];
+  user: User;
+  payment: Payment[];
+  metadata: Record<string, unknown>; // or specific metadata type if known
+  status: string;
+  responses: {
+    email: string;
+    name: string;
+    location: {
+      optionValue: string;
+      value: string;
+    };
+  };
+}
+
+interface Attendee {
+  email: string;
+  name: string;
+  timeZone: string;
+  locale: string;
+}
+
+interface User {
+  email: string;
+  name: string;
+  timeZone: string;
+  locale: string;
+}
+
+interface Payment {
+  id: number;
+  success: boolean;
+  paymentOption: string;
+}
+
+const createBooking = async (opts: CreateBookingOptions) => {
+  const calApiUrl = `https://api.cal.com/v1/bookings?apiKey=${opts.calApiKey}`;
+
+  try {
+    const response = await axios.post<Booking>(calApiUrl, opts);
+
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return undefined;
+  }
+};
+
 function addMinutes(date: Date, minutes: number) {
   date.setMinutes(date.getMinutes() + minutes);
   return date.toISOString();
@@ -105,7 +205,35 @@ export const checkAvailabilityHandler = async ({
     });
   }
 
-  await db.checkIns.create({
+  const booking = await createBooking({
+    calApiKey: user.apiKey,
+    eventTypeId: input.eventId,
+    start: meetingTime.contactDateTime,
+    end: addMinutes(new Date(meetingTime.contactDateTime), event.length!),
+    responses: {
+      name: user.name!,
+      email: user.email!,
+      location: "Online",
+    },
+    metadata: {
+      clicIs: "true",
+    },
+    language: "en",
+    timeZone: user.timeZone,
+    title: event.title,
+    description: "Meet up with your contact",
+    status: "PENDING",
+    smsReminderNumber: null,
+  });
+
+  if (!booking) {
+    throw new TRPCError({
+      code: "INTERNAL_SERVER_ERROR",
+      message: "Failed to create booking",
+    });
+  }
+
+  const checkIn = await db.checkIns.create({
     data: {
       status: "PENDING",
       title: event.title,
@@ -117,6 +245,13 @@ export const checkAvailabilityHandler = async ({
       endDate: addMinutes(new Date(meetingTime.contactDateTime), event.length!),
     },
   });
+
+  if (!checkIn) {
+    throw new TRPCError({
+      code: "INTERNAL_SERVER_ERROR",
+      message: "Failed to create check-in",
+    });
+  }
 
   return meetingTime;
 };
--- src/server/auth.ts
+++ src/server/auth.ts
@@ -40,6 +40,7 @@ export const authOptions: NextAuthOptions = {
         username: user.username,
         id: user.id,
         calId: user.calId,
+        apiKey: user.apiKey,
         timeZone: user.timeZone,
       },
     }),
